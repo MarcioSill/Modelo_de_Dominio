2c2d3a7167a98a2167700561ecc5b0749d8622d7
MarcioSill <marcio.sill@hotmail.com>
2025-04-02 17:30:33 -0300

    Criacao tabela categoria e participante

---
+++ src/main/java/com/marciosill/modelodedominio/entities/Participante.java
@@ -0,0 +1,50 @@
+package com.marciosill.modelodedominio.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_participantes")
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true) //para se um email unico
+    private String email;
+
+    public Participante() {
+    }
+
+    public Participante(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
